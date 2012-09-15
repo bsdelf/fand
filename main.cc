da41984ee76275d511d1efd5f1443821d227a2e8
Yanhui Shen <shen.elf@gmail.com>
2012-09-15 19:52:49 +0800

    * state machine

--- main.cc
+++ main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
+#include <array>
 #include <algorithm>
-#include <vector>
+#include <ctime>
+#include <locale>
 using namespace std;
 
 #include <unistd.h>
@@ -10,6 +13,7 @@ using namespace std;
 #include <sys/resource.h>
 
 const char* const ctl_thermal = "dev.acpi_ibm.0.thermal";
+const char* const ctl_fan = "dev.acpi_ibm.0.fan";
 const char* const ctl_fanlevel = "dev.acpi_ibm.0.fan_level";
 
 /*
@@ -23,93 +27,237 @@ const char* const ctl_fanlevel = "dev.acpi_ibm.0.fan_level";
  * 8. UltraBay battery
  */
 
+static string DateTime(const char* fmt = "%Y.%m.%d %T")
+{
+    // buffer size is limited to 100
+    char buf[100];
+    std::time_t t = std::time(NULL);
+    size_t len = std::strftime(buf, sizeof(buf), fmt, std::localtime(&t));
+    return string(buf, len);
+}
+
+static bool FetchThermal(array<int, 8>& thermal)
+{
+    // verify, fetch, normalize
+    size_t thermalLen = 0;
+    if (sysctlbyname(ctl_thermal, nullptr, &thermalLen, nullptr, 0) != 0)
+        return false;
+    if (thermalLen != sizeof(thermal))
+        return false;
+    if (sysctlbyname(ctl_thermal, thermal.data(), &thermalLen, nullptr, 0) != 0)
+        return false;
+    replace_if(thermal.begin(), thermal.end(),
+               [](int t)->bool { return t >= 255; },
+               -1);
+    return true;
+}
+
+enum class EmHandleRet
+{
+    Ok,
+    Err,
+    Out
+};
+
+struct IHandler
+{
+    virtual ~IHandler() { }
+
+    virtual EmHandleRet Handle(int t) = 0;
+    virtual bool InRange(int t) const = 0;
+
+    static bool TrySwitch(int level)
+    {
+        // fetch
+        int prevLevel = 0;
+        size_t levelLen = sizeof(prevLevel);
+        if (sysctlbyname(ctl_fanlevel, &prevLevel, &levelLen, nullptr, 0) != 0)
+            return false;
+        if (prevLevel == level)
+            return true;
+
+        // update
+        cout << DateTime() << " " << prevLevel << " => " << level;
+        if (sysctlbyname(ctl_fanlevel, nullptr, 0, &level, sizeof(level)) == 0) {
+            cout << endl;
+            return true;
+        } else {
+            cout <<  " Failed!" << endl;
+            return true;
+        }
+    }
+
+    static int HOLD_TIMES;
+    static int MORE;
+};
+int IHandler::HOLD_TIMES = 0;
+int IHandler::MORE = 0;
+
+// (min, max]
+
+struct LV1Handler: public IHandler
+{
+    virtual EmHandleRet Handle(int t)
+    {
+        if (t > max+MORE)
+            return EmHandleRet::Out;
+
+        return TrySwitch(1) ? EmHandleRet::Ok : EmHandleRet::Err;
+    }
+
+    virtual bool InRange(int t) const
+    {
+        return (t <= max);
+    }
+
+    int max;
+};
+
+struct LV2Handler: public IHandler
+{
+    virtual EmHandleRet Handle(int t)
+    {
+        if (t > max+MORE) {
+            escapeTimes = 0;
+            return EmHandleRet::Out;
+        }
+
+        if (t <= min) {
+            if (++escapeTimes >= HOLD_TIMES) {
+                escapeTimes = 0;
+                return EmHandleRet::Out;
+            }
+        }
+
+        return TrySwitch(2) ? EmHandleRet::Ok : EmHandleRet::Err;
+    }
+
+    virtual bool InRange(int t) const
+    {
+        return (t > min && t <= max);
+    }
+
+    int max;
+    int min;
+
+private:
+    int escapeTimes = 0;
+};
+
+struct LV3Handler: public IHandler
+{
+    virtual EmHandleRet Handle(int t)
+    {
+        if (t <= min) {
+            if (++escapeTimes >= HOLD_TIMES) {
+                escapeTimes = 0;
+                return EmHandleRet::Out;
+            }
+        }
+
+        return TrySwitch(3) ? EmHandleRet::Ok : EmHandleRet::Err;
+    }
+
+    virtual bool InRange(int t) const
+    {
+        return (t > min);
+    }
+
+    int min;
+
+private:
+    int escapeTimes = 0;
+};
+
 int main()
 {
-    int level = 1;
-    int thermal[8];
-    size_t levelLen = sizeof(level);
-    size_t thermalLen = sizeof(thermal);
-    const int thermalCount = sizeof(thermal)/sizeof(thermal[0]);
-    const int hold = 10*1000;   // ms
+    array<int, 8> thermal;
+    const int hold = 30*1000;   // ms
     const int tick = 500;       // ms
-    const int holdtimes = hold/tick;
     const int nice = -10;
+    IHandler::MORE = 5;
+    IHandler::HOLD_TIMES = hold/tick;
 
+    // renice
     if (setpriority(PRIO_PROCESS, 0, nice) != 0) {
         cout << "renice myself failed!" << endl;
         return 1;
     }
 
-    //cout << sysctlbyname(ctl_fanlevel, nullptr, 0, &level, sizeof(level)) << endl;
-    //return 0;
+    // prepare
+    LV1Handler h1; h1.max = 40;
+    LV2Handler h2; h2.min = 40; h2.max = 50;
+    LV3Handler h3; h3.min = 50;
+    IHandler* const handlers[3] = { &h1, &h2, &h3 };
 
-    {
-        size_t len = 0;
-        int err = sysctlbyname(ctl_thermal, nullptr, &len, nullptr, 0);
-        if (err != 0) {
-            cout << "sysctl failed!" << endl;
-            return 1;
-        } else if (len != sizeof(thermal)) {
-            cout << "size mismatch!" << endl;
-            return 1;
+    auto FnPickHandler = [&handlers](int t)->IHandler* {
+        for (IHandler* h: handlers) {
+            if (h->InRange(t))
+                return h;
         }
+        return nullptr;
+    };
+
+    // switch to manual mode
+    while (true) {
+        int err = 0;
+        int val = 1;
+
+        err = sysctlbyname(ctl_fanlevel, nullptr, 0, &val, sizeof(val));
+        if (err != 0) goto label_failed;
+
+        val = 0;
+        err = sysctlbyname(ctl_fan, nullptr, 0, &val, sizeof(val));
+        if (err != 0) goto label_failed;
+
+        break;
+
+label_failed:
+        cout << "can't switch to manual mode!" << endl;
+        return 1;
     }
 
-    for (int lv1times = 0, lv2times = 0; ; ) {
+    // begin to work
+    cout << DateTime() << " begin" << endl;
+    for (IHandler* h = FnPickHandler(10); h != nullptr; ) {
+        if (!FetchThermal(thermal))
+            break;
+
+        int cpu = thermal[0];
+        EmHandleRet ret = h->Handle(cpu); 
+        switch (ret) {
+        case EmHandleRet::Out: {
+            IHandler* _h = FnPickHandler(cpu);
+            // avoid busy Handle()
+            if (_h != h) {
+                h = _h;
+                continue;
+            }
+        }
+            break;
+
+        case EmHandleRet::Ok:
+            break;
+
+        case EmHandleRet::Err:
+        default: {
+            h = nullptr;
+            continue;
+        }
+            break;
+        }
+
         usleep(tick*1000);
-
-        int err = sysctlbyname(ctl_thermal, thermal, &thermalLen, nullptr, 0);
-
-        // 255 => -1
-        replace_if(thermal, thermal+thermalCount,
-                   [](int thermal)->bool { return thermal >= 255; },
-                   -1);
-
-        // fetch current level
-        err = sysctlbyname(ctl_fanlevel, &level, &levelLen, nullptr, 0);
-        if (err != 0) {
-            cout << "fetch level failed!" << endl;
-            return 1;
-        }
-
-        // adjust it according to CPU
-        auto& max = thermal[0];//*max_element(thermal, thermal+thermalCount);
-        if (max < 40) {
-            if (level != 1) {
-                if (++lv1times < holdtimes)
-                    continue;
-                else
-                    lv1times = 0;
-
-                cout << level << " => 1" << endl;
-                level = 1;
-                sysctlbyname(ctl_fanlevel, nullptr, 0, &level, sizeof(level));
-            }
-        } else if (max > 48) {
-            lv1times = 0;
-            lv2times = 0;
-
-            if (level != 3) {
-                cout << level << " => 3" << endl;
-                level = 3;
-                sysctlbyname(ctl_fanlevel, nullptr, 0, &level, sizeof(level));
-            }
-        } else {
-            lv1times = 0;
-            if (level > 2) {
-                if (++lv2times < holdtimes)
-                    continue;
-                else
-                    lv2times = 0;
-            }
-
-            if (level != 2) {
-                cout << level << " => 2" << endl;
-                level = 2;
-                sysctlbyname(ctl_fanlevel, nullptr, 0, &level, sizeof(level));
-            }
-        }
     }
+    cout << DateTime() << " end" << endl;
+
+    // recover to automatic mode
+    {
+        int val = 1;
+        if (sysctlbyname(ctl_fan, nullptr, 0, &val, sizeof(val)) != 0)
+            return 1;
+    }
+    cout << DateTime() << " recovered" << endl;
 
     return 0;
 }
